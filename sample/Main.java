5d57eba665374a8f7fc42b6140aefc02998f41c4
elviskankola1 <elviskankola1@gmail.com>
2019-06-24 18:03:43 +0200

    update titre de la fenetre

--- sample/Main.java
+++ sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("ecran principale");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
