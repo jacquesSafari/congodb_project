acaec3c799678937f8b4ac8c80ec51724ff02588
elsa.m <manangelsa07@gmail.com>
2019-06-24 16:36:49 +0200

    creation table

--- Sgbd15/src/sgbd15/Kernerl/Table.java
+++ Sgbd15/src/sgbd15/Kernerl/Table.java
@@ -1,9 +1,5 @@
 package sample.Kernerl;
 
-/*
-*Ce fichier contient tous ce qui concerne la creation
-*et les operations sur des tables
-*/
 
 
 import javax.xml.parsers.DocumentBuilder;
@@ -24,30 +20,17 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
- //pour les collections
  import java.util.List;
  import java.util.ArrayList;
- //pour les flux d'entree
  import java.util.Scanner;
- //pour les io
  import java.io.*;
-
- //class
 public class Table{
-     //fields
-     
      private String nom;
-     //this table belongs to this DB     t<Field>();
      private List<Field> champs = new ArrayList<Field>();
 
-     //constructer
      public Table(String nom){
          this.nom = nom;
-         //this.id = ++Table.nbreInstance;
      }     
-
-    //default construct
     public Table(){
         
     }
@@ -66,7 +49,7 @@ public class Table{
      }
 
      public void createTableFieldFolder(){
-         String chemin = Database.path+"Databases\\tableFields\\";
+         String chemin = Database.path+"Databases\\fieldsTable\\";
          File tableFields= new File(chemin);
          if(!(tableFields.isDirectory())){
              tableFields.mkdir();
@@ -75,54 +58,42 @@ public class Table{
      
      public boolean createTable(String nomDb){       
         
-        System.out.println("Tapez une requete de ce type:\n==========================");
-        System.out.println("*CREATE TABLE nom ( champ1 type1 champ2 type2 )*\n");
-
-        //recuperation de la requete
+        System.out.println("Tapez une requete ici :\n==========================");
+        System.out.println(" exemple *CREATE TABLE nom ( champ1 type1 champ2 type2 )*\n");
         Scanner sc = new Scanner(System.in);
         String requete = sc.nextLine();
-
-        //transformations de la requete en tableau
         String separateur = " ";        
         String[] t_requete = requete.split(separateur);
 
         int size_tab = t_requete.length;
-        //le tableau qui contiendra les champs et les types
         String[] t_champs = new String[size_tab-5];
-        //un tableau  uniquement pour les types pour y ajouter des contraintes
         String[] t_types = new String[(size_tab-5)/2];
         
-        //verifions si le mot cle est correct
         if(!((t_requete[0]+" "+t_requete[1]).equals("CREATE TABLE"))) {
-            System.out.println("Mot cle inconnue!!");
+            System.out.println("Mot inconnue!!");
             System.exit(0);
         }
 
-        //verifions si les espaces entre les() sont respectes
         if(!((t_requete[3]+t_requete[size_tab-1]).equals("()"))){
-            System.err.println("Error requete!\nveuillez mettre des espaces => nom_table ( nomchamp type1 )");
+            System.err.println("Mauvaise requete !\n Mettez des espaces => nom_table ( nomchamp type1 )");
             System.exit(0);
         }
 
-        //recuperer les champs et leurs types dans un tableau
         for(int i = 4 ;i < (size_tab-1) ;i++){
             t_champs[i-4] = t_requete[i];
         }       
 
-        //verifion si le nombre d'elements entre les() est paire
         int size_tab_champ = t_champs.length;
         if(!(size_tab_champ%2 == 0)){
-            System.out.println("Chaque champ doit etre lie a un type");
+            System.out.println("Chaque champ doit avoir un type");
             System.exit(0);
         }
-        //recuperons les types
         int indiceOfType = 0;
         for(int k = 0; k < (size_tab_champ-1);k+=2){
             t_types[indiceOfType] = t_champs[k+1];                        
             indiceOfType++;
         }       
         
-        //verifons si les types fourinis sont valable(disponible)
         for (String type : t_types) {
             if(!(isValidType(type))){
                 System.out.println("Error! Le type: "+type+" n'est pas disponible!");
@@ -130,37 +101,32 @@ public class Table{
             }
         }       
         
-        //=============si tout est bon passons a xml================================
         String chemin = Database.path+"Databases\\"+nomDb+"\\"+t_requete[2]+".xml";
         File table = new File(chemin);
         if(!(table.exists())){
             try {
-                //========le constructeur des document=================================
                  DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();                
                  DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
                  Document doc = dBuilder.newDocument();
-
-                 //==========l'element racine================
                  Element rootElement = doc.createElement("table_"+t_requete[2]);
                  doc.appendChild(rootElement);  
 
-                 //la terminaison de tous les fichiers contenant les champs
                 String fileToSaveFields = "_file.txt";
                 createTableFieldFolder();
-                PrintWriter impression = new PrintWriter(new FileWriter(Database.path+"Databases\\tableFields\\"+t_requete[2]+fileToSaveFields));
+                PrintWriter impression = new PrintWriter(new FileWriter(Database.path+"Databases\\fieldsTable\\"+t_requete[2]+fileToSaveFields));
             
                 for(int j = 0;j < (t_champs.length-1);j=j+2){   
                     impression.println(t_champs[j]);
                     impression.println(t_champs[j+1]);               
                 }
                 impression.flush();
-                 //=========finaliser toute chose==================
+                 //===========================================================
                 TransformerFactory transformerFactory = TransformerFactory.newInstance();                
                 Transformer transformer = transformerFactory.newTransformer();                
                 DOMSource source = new DOMSource(doc);
                 StreamResult result = new StreamResult(new File(chemin));                
                 transformer.transform(source, result);                
-                System.out.println("La table "+t_requete[2]+" est creee aves succes dans la BDD "+nomDb);               
+                System.out.println("La table "+t_requete[2]+" est creee dans la BDD "+nomDb);               
                 
             } catch (Exception e) {
                  e.printStackTrace();
@@ -172,10 +138,8 @@ public class Table{
             return false;
         }
     }
-    
-    //=========methode de verification============
+    //=========================================================
     public boolean isValidType(String typeGiven){
-       //types diponible
        String[] types ={"int","string","double","float","short"};
        for (String var : types) {
            if(var.equals(typeGiven)){
@@ -184,14 +148,12 @@ public class Table{
        }
        return false;
     }
-
-     //table operations     
+   
     public void insertData(String nomDb,String nomTable){
          
         String chemin = checkingFolderAndFile(nomDb,nomTable);
         
-        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();         
-        //essayons d'acceder au fichier et d'y inserer des donnees
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); 
         try {        
             int id = 1;
             final DocumentBuilder builder = factory.newDocumentBuilder();  
@@ -215,8 +177,7 @@ public class Table{
                         
             racine.appendChild(conteneur);
 
-            //connection au fichier et ouverture en mode lecture
-            BufferedReader flux = new BufferedReader(new FileReader(Database.path+"Databases\\tableFields/"+nomTable+"_file.txt"));
+            BufferedReader flux = new BufferedReader(new FileReader(Database.path+"Databases\\fieldsTable/"+nomTable+"_file.txt"));
             //======================================
             String line;
             int count = 0;
@@ -226,7 +187,6 @@ public class Table{
                count++;               
             }            
             
-            //tableau des champs et des types
             String[] champEtType = new String[count];
             String phrase = "";
             for (String ligne : fields) {
@@ -236,7 +196,6 @@ public class Table{
             for(int j = 0;j < (champEtType.length-1);j=j+2){
                 Scanner sc = new Scanner(System.in);
                 System.out.println("Entrez le "+champEtType[j]); 
-                //valeur du champ
                 String value = sc.nextLine();  
                 final Element champ = document.createElement(champEtType[j]);
                 champ.setAttribute("type", champEtType[j+1]);
@@ -256,17 +215,10 @@ public class Table{
 
             final StreamResult sortie = new StreamResult(new File(chemin));           
 
-                    
-
-            //formatage
-
+            
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
             transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-
-                
-
-            //sortie
 
             transformer.transform(source, sortie);  
 
@@ -308,8 +260,6 @@ public class Table{
 
          return "";
      }
-
-     //update item
      public void updateData(String nomDb, String nomTable){
         String chemin =  checkingFolderAndFile(nomDb,nomTable);
          try {
@@ -317,19 +267,17 @@ public class Table{
              DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
              DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
              Document doc = docBuilder.parse(chemin);
-             // Get the root element
-            // Node data= doc.getFirstChild();
+             //===============================================================
              final Element data = doc.getDocumentElement();
              NodeList nList = data.getElementsByTagName(nomTable);
              Scanner sc = new Scanner(System.in);
-             System.out.println("Entrez le champ dont vous voulez changer la valeur:");
+             System.out.println("Entrez le champ ou vous voulez changer la valeur:");
              String field = sc.nextLine();
-             System.out.println("Entrer la nouvelle valeure");
+             System.out.println("Entrer la nouvelle valeur : ");
              String value = sc.nextLine();
              System.out.println("id:");
              String idNode = sc.nextLine();
              char idItem = idNode.charAt(0);
-             //bouclons
              boolean isDone = false;
              for (int v = 0; v < nList.getLength(); v++) {
                  Node nNode = nList.item(v);
@@ -346,12 +294,11 @@ public class Table{
                  }
              }
              if(isDone){
-                 System.out.println("Done");
+                 System.out.println("Bien");
              }
              else{
-                 System.out.println("id not found");
+                 System.out.println("Pas trouve");
              }
-             //write content in xml file
              writeContentInXmlFile(chemin,doc);
 
          } catch (ParserConfigurationException e) {
@@ -365,7 +312,7 @@ public class Table{
              e.printStackTrace();
          }
      }
-     //methode delete
+     //===============================================================
      public void deleteData(String nomDb, String nomTable){
         String chemin = checkingFolderAndFile(nomDb,nomTable);
          try {
@@ -397,12 +344,11 @@ public class Table{
                  }
              }
              if(isDone){
-                 System.out.println("Done");
+                 System.out.println("Fait");
              }
              else{
-                 System.out.println("id not found");
+                 System.out.println("id non trouve");
              }
-            //write content in xml file
              writeContentInXmlFile(chemin,doc);
 
          } catch (ParserConfigurationException e) {
