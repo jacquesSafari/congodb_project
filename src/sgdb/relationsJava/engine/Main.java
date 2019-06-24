9d6b2454c141ffccf6bcdba0480938dd6109207e
savalone47 <savalonescott47@gmail.com>
2019-06-25 00:03:07 +0200

    update the main function

--- src/sgdb/relationsJava/engine/Main.java
+++ src/sgdb/relationsJava/engine/Main.java
@@ -6,6 +6,31 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.io.File;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import org.w3c.dom.Comment;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
 import sgdb.relationsJava.engine.core.operation.TableOperation;
 import sgdb.relationsJava.engine.persistance.Attribut;
 import sgdb.relationsJava.engine.persistance.Database;
