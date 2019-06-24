bb00f692f0edd76c5704409a39a7cf7e77937651
elviskankola1 <elviskankola1@gmail.com>
2019-06-24 18:00:22 +0200

    fichier contenant les champs dune table

---
+++ sample/core/Field.java
@@ -0,0 +1,41 @@
+package sample.core;
+
+public class Field{
+
+    private String nom;
+    private String type;
+
+    public Field()
+    {
+
+    }
+
+    public String getNom()
+    {
+
+        return this.nom;
+
+    }
+
+    public String getType()
+    {
+
+        return this.type;
+
+    }
+
+    public void setNom(String nom)
+    {
+
+        this.nom = nom;
+
+    }
+
+    public void setType(String type)
+    {
+
+        this.type = type;
+
+    }
+    
+}
